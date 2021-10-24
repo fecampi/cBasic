17f9b0bd0a60455b1a6a3aafe9aad765aa8f0218
Felipe Campinho <felipe.campinho@mirakulo.com>
2021-10-24 18:13:00 -0300

    add dinamic

---
+++ c++/12-include.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib/functions.h"
+
+int main()
+{
+    printMensage();
+    return 0;
+}
---
+++ c++/13-dynamic allocation.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    printMensage();
+    return 0;
+}
